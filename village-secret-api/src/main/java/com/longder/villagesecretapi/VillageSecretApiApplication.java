493b7d8c0f0753b59a67d2c16381b3b2574974b2
Longder <longder1989@qq.com>
2021-09-03 17:39:11 +0800

    Initial commit

---
+++ village-secret-api/src/main/java/com/longder/villagesecretapi/VillageSecretApiApplication.java
@@ -0,0 +1,13 @@
+package com.longder.villagesecretapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillageSecretApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillageSecretApiApplication.class, args);
+    }
+
+}
---
+++ village-secret-web/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <el-button>
+    我是 ElButton
+  </el-button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
